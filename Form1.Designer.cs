2f442194751b4836ecd7d0e73fe0713a5ce47879
keynesproject <keynesproject@gmail.com>
2016-06-17 22:14:34 +0800

    test tortoisegit save account function

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 421);
+            this.ClientSize = new System.Drawing.Size(442, 263);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
